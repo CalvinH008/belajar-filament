be9d4059e0e7adca06460918e89c256c934385d9
CalvinH008 <tololcapung@gmail.com>
2026-02-17 12:43:42 +0700

    instal filament

---
+++ app/Filament/Resources/Students/Schemas/StudentInfolist.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Students\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class StudentInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('user_id')
+                    ->numeric(),
+                TextEntry::make('classroom_id')
+                    ->numeric(),
+                TextEntry::make('nism'),
+                TextEntry::make('phone_number'),
+                TextEntry::make('gender'),
+                TextEntry::make('profile_picture'),
+                TextEntry::make('created_at')
+                    ->dateTime(),
+                TextEntry::make('updated_at')
+                    ->dateTime(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Students/Tables/StudentsTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\Students\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class StudentsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('classroom_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('nism')
+                    ->searchable(),
+                TextColumn::make('phone_number')
+                    ->searchable(),
+                TextColumn::make('gender'),
+                TextColumn::make('profile_picture')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
